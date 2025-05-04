a1a80c0f8f23c1313674860634fbcb12632e1cfd
PCykana <peter-cykana@live.nmit.ac.nz>
2025-05-04 22:24:21 +1200

    bigBoard is has more consistent spacing when space strings are used as blanks

--- TripleT.js
+++ TripleT.js
@@ -1,15 +1,15 @@
 let playerOneSymbol = 'X'
 let playerTwoSymbol = 'O'
+/*
 const board = [['','X',''],['O','',''],['','','O']]
 console.log(board[0])
 console.log(board[1])
 console.log(board[2])
-
-/* experiments with a big board
-const bigBoard = [[' ','|',' ','|','O'],['—','X','—',' ','—'],[' ','|','X','|',' '],['—',' ','—',' ','—'],['O','|',' ','|',' ']]
-console.log(bigBoard[0])
-console.log(bigBoard[1])
-console.log(bigBoard[2])
-console.log(bigBoard[3])
-console.log(bigBoard[4])
 */
+/* experiments with a big board */
+const bigBoard = [[' ','|',' ','|',' '],['—','+','—','+','—'],[' ','|',' ','|',' '],['—','+','—','+','—'],[' ','|',' ','|',' ']]
+console.log(...bigBoard[0])
+console.log(...bigBoard[1])
+console.log(...bigBoard[2])
+console.log(...bigBoard[3])
+console.log(...bigBoard[4])
