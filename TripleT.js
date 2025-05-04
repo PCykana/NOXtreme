b90e813806f7ae6345258e48997e08fbbb53fab9
PCykana <peter-cykana@live.nmit.ac.nz>
2025-05-04 22:00:08 +1200

    displaying stuff the hard way

--- TripleT.js
+++ TripleT.js
@@ -1,3 +1,8 @@
 let playerOneSymbol = 'X'
 let playerTwoSymbol = 'O'
-const board = [['','',''],['','',''],['','','']]
+const board = [[' ','|',' ','|','O'],['-',' ','-',' ','-'],[' ','|','X','|',' '],['-',' ','-',' ','-'],['O','|',' ','|',' ']]
+console.log(...board[0])
+console.log(...board[1])
+console.log(...board[2])
+console.log(...board[3])
+console.log(...board[4])
