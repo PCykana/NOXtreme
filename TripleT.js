237d55025ed8960fa366fb35c7f2d0ee60d2ab84
PCykana <peter-cykana@live.nmit.ac.nz>
2025-05-10 11:14:33 +1200

    using array destructuring to update the display array with variables

--- TripleT.js
+++ TripleT.js
@@ -1,15 +1,15 @@
 let playerOneSymbol = 'X'
 let playerTwoSymbol = 'O'
-/*
-const board = [['','X',''],['O','',''],['','','O']]
-console.log(board[0])
-console.log(board[1])
-console.log(board[2])
-*/
+
+const board = [[' ','X',' '],['O',' ',' '],[' ',' ','O']]
+let [[p1, p2, p3],[p4, p5, p6],[p7, p8, p9]] = board
+console.log(p2)
 /* experiments with a big board */
-const bigBoard = [[' ','|',' ','|',' '],['—','+','—','+','—'],[' ','|',' ','|',' '],['—','+','—','+','—'],[' ','|',' ','|',' ']]
+const bigBoard = [[p1,'|', p2,'|', p3],['—','+','—','+','—'],[p4,'|', p5,'|', p6],['—','+','—','+','—'],[p7,'|', p8,'|', p9]]
 console.log(...bigBoard[0])
 console.log(...bigBoard[1])
 console.log(...bigBoard[2])
 console.log(...bigBoard[3])
 console.log(...bigBoard[4])
+
+
