f77c70b5248b143e5ca9b7777542277192d0f3c6
PCykana <peter-cykana@live.nmit.ac.nz>
2025-05-10 13:14:20 +1200

    checking position and attempting to iterate

--- TripleT.js
+++ TripleT.js
@@ -1,15 +1,45 @@
 let playerOneSymbol = 'X'
 let playerTwoSymbol = 'O'
+const board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 
-const board = [[' ','X',' '],['O',' ',' '],[' ',' ','O']]
-let [[p1, p2, p3],[p4, p5, p6],[p7, p8, p9]] = board
-console.log(p2)
+
+//function createPosition () {
+
+for (let i = 0; i < 9; i++) {
+    
+    let row = 0
+    let column = 0
+    let checkPosition = true
+        while (checkPosition) {
+        column = Math.floor(Math.random() * 3)
+        row = Math.floor(Math.random() * 3)
+        console.log(row,column)
+        if(board[row][column] == ' '){
+            checkPosition = false
+        } else {
+            checkPosition = true
+        }
+        }
+        if(i % 2 == 0) {
+            board[row][column] = 'X'
+        } else if(i % 2 == 1) {
+            board[row][column] = 'O'
+        } else { console.log('error')}
+
+   setTimeout(displayBoard(board), '3000')
+    
+}
+
+
 /* experiments with a big board */
-const bigBoard = [[p1,'|', p2,'|', p3],['—','+','—','+','—'],[p4,'|', p5,'|', p6],['—','+','—','+','—'],[p7,'|', p8,'|', p9]]
-console.log(...bigBoard[0])
-console.log(...bigBoard[1])
-console.log(...bigBoard[2])
-console.log(...bigBoard[3])
-console.log(...bigBoard[4])
-
+function displayBoard(gameState){
+    let [[p1, p2, p3],[p4, p5, p6],[p7, p8, p9]] = gameState
+    const bigBoard = [[p1,'|', p2,'|', p3],['—','+','—','+','—'],[p4,'|', p5,'|', p6],['—','+','—','+','—'],[p7,'|', p8,'|', p9]]
+    console.log(...bigBoard[0])
+    console.log(...bigBoard[1])
+    console.log(...bigBoard[2])
+    console.log(...bigBoard[3])
+    console.log(...bigBoard[4])
+    }
+    
 
