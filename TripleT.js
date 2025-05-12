a52f410249fd553ec9fdf6025fe91a99e635cfa1
PCykana <peter-cykana@live.nmit.ac.nz>
2025-05-12 23:00:39 +1200

    figured out how to refactor the column check into ES6 as well as removing the iteration from both the row and column checks

--- TripleT.js
+++ TripleT.js
@@ -2,29 +2,38 @@ const playerOneSymbol = 'X' // creates a constant to hold the symbol for player 
 const playerTwoSymbol = 'O' // creates a constant to hold the symbol for player 2
 let player = '' // creates an empty string placeholder to swap between player identification for player turn output
 let stamp = '' // creates an empty string variable placeholder to swap between player symbols when updating the board state
-let finalMessage = ''
+let finalMessage = '' // creates an empty string variable placeholder to update the message shown below the board for each iteration
 const board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']] // not null but it's the empty(space character filled) array of arrays to keep track of the game board
-// [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']] <-- this stays for quick copy/paste re-sets if i need to troubleshoot a function
-const displayBoard = ([[p1, p2, p3],[p4, p5, p6],[p7, p8, p9]] = gameState, message = '') => console.log(`\n ${p1} | ${p2} | ${p3}\n — + — + —\n ${p4} | ${p5} | ${p6} \n — + — + —\n ${p7} | ${p8} | ${p9}\n\n${message}`)
-const playerCheck = (i) => (i % 2 == 0) ? (player = 'Player 1', stamp = playerOneSymbol) : (i % 2 == 1) ? (player = 'Player 2', stamp = playerTwoSymbol) : console.log('Error') // switches player based on modulo operator of current loop iteration passed in as i
+// [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']] <-- this stays for quick copy/paste re-sets if I need to troubleshoot a function
 
-const winner = (gameBoard, symbol) => // makes a function for going through win states
-    rowCheck(gameBoard[0], symbol, 1) || rowCheck(gameBoard[1], symbol, 2) || rowCheck(gameBoard[2], symbol, 3) ? true // checks each row to see if there is a win
-    : columnCheck(gameBoard, symbol)? true // checks each column to see if there is a win
-    : diagCheck(gameBoard, symbol) ? true // checks both diagonals for a win returns true 
+// displayBoard destructures the board array as the gameState parameter, and appends a message at the end of the displayed board, if no message is passed it defaults to a null space.
+const displayBoard = ([[p1, p2, p3],[p4, p5, p6],[p7, p8, p9]] = gameState, message = '') => console.log(`\n ${p1} | ${p2} | ${p3}\n — + — + —\n ${p4} | ${p5} | ${p6} \n — + — + —\n ${p7} | ${p8} | ${p9}\n\n${message}`)
+
+// switches player based on modulo operator of current loop iteration passed in as i by changing the global variabls player and stamp.
+const playerCheck = (i) => (i % 2 == 0) ? (player = 'Player 1', stamp = playerOneSymbol) : (i % 2 == 1) ? (player = 'Player 2', stamp = playerTwoSymbol) : console.log('Error') 
+
+const winner = (gameBoard, position, symbol) => // makes a function wrangler(i don't know if there's a proper term for this) for going through win states
+    rowCheck(gameBoard[position[0]], symbol, position[0]) ? true // checks the row containing the last move to see if there is a win
+    : columnCheck(gameBoard, position[1], symbol) ? true // checks the column containing the last move to see if there is a win
+    : diagCheck(gameBoard, symbol) ? true // checks both diagonals for a win
     : false // retruns false to continue game
 
-const rowCheck = (arr, symbol, row) => symbol== arr[0] && arr[0] == arr[1] && arr[1] == arr[2] ? (finalMessage =`${player} wins on row ${row}!`, true) : false // checks to see if all the characters in a row match the current player character
-const diagCheck = (arr, symbol) => symbol == arr[1][1] && ((arr[1][1] == arr[0][0] && arr[0][0] == arr[2][2]) || (arr[1][1] == arr[0][2] && arr[0][2] == arr[2][0])) ? (finalMessage =`${player} wins on a diagonal!`, true) : false // checks to see if all the characters in a diagonal match the current player character
+// checks to see if all the characters in a specified row matche the current player character
+const rowCheck = (arr, symbol, row) => symbol == arr[0] && arr[0] == arr[1] && arr[1] == arr[2] ? (finalMessage =`${player} wins on row ${row+1}!`, true) : false
 
+// checks to see if all the characters in a specified column match the current player symbol
+const columnCheck = (arr, col, symbol) => symbol == arr[0][col] && arr[0][col] == arr[1][col] && arr[1][col] == arr[2][col] ? (finalMessage =`${player} wins on column ${col+1}!`, true) : false
+
+// checks to see if all the characters in a diagonal match the current player character
+const diagCheck = (arr, symbol) => symbol == arr[1][1] && ((arr[1][1] == arr[0][0] && arr[0][0] == arr[2][2]) || (arr[1][1] == arr[0][2] && arr[0][2] == arr[2][0])) ? (finalMessage =`${player} wins on a diagonal!`, true) : false 
 
 // the game call it self
 displayBoard(board)    
-
+// the main for loop- each iteration is a turn
 for (let i = 0; i < 9; i++) {
-    let row = 0
-    let column = 0
-    let checkPosition = true
+    let row = 0 // creates an internally scoped variable for row
+    let column = 0 // creates an internally scoped variable for column
+    let checkPosition = true // creates an assignable condition to regulate the while loop so it knows when to stop generating new numbers
     playerCheck(i)
 
     console.log(`It's ${player}'s turn (${stamp})\n`) 
@@ -40,7 +49,7 @@ for (let i = 0; i < 9; i++) {
     board[row][column] = stamp
    
 
-    if(winner(board, stamp)){
+    if(winner(board, [row, column], stamp)){
         break
     }  else if(drawCheck(board)){
         break
@@ -54,6 +63,20 @@ for (let i = 0; i < 9; i++) {
 displayBoard(board, finalMessage)
 
 
+function drawCheck(arr){
+    let isADraw = true
+    for(let d = 0; d <arr.length; d++) {
+        if(arr[d].includes(' ')){
+        isADraw = false
+        break
+        }else{
+            finalMessage = '*spooky western music* This game ends in a draw pardner.' 
+        }
+    }
+    return isADraw
+}
+
+/*!SECTION original ES5 function versions and non-es6 refactors for functions that have been changed to ES6
 
 // displayBoard function creates a visually appealing board by destructuring the game board, passed in as gameState,
 // and places the values in a larger arry with character formatting to visually create rows and columns.
@@ -118,8 +141,7 @@ function rowCheck(arr, player) {
     }
 }
 */
-
-
+/*
 function columnCheck(arr,symbol){
     let isAWin = false   
     for (let c = 0; c < arr.length; c++) {
@@ -131,7 +153,7 @@ function columnCheck(arr,symbol){
     }
     return isAWin
 }
-//const columnCheck = (arr, player, isAWin = false)
+*/
 
 /*
 function diagCheck(arr, player){
@@ -143,15 +165,4 @@ function diagCheck(arr, player){
 }
 */
 
-function drawCheck(arr){
-    let isADraw = true
-    for(let d = 0; d <arr.length; d++) {
-        if(arr[d].includes(' ')){
-        isADraw = false
-        break
-        }else{
-            finalMessage = '*spooky western music* This game ends in a draw pardner.' 
-        }
-    }
-    return isADraw
-}
+const checkRow = (row) => board[row][column] == board[row][column-1] && board[row][column] == board[row][column+1]
